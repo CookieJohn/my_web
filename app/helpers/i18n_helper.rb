85c615fdccba705f0abe90ae5bb70b50d386e567
john <johnwu2613@gmail.com>
2017-04-08 23:27:35 +0800

    done-basic-job-section

--- app/helpers/i18n_helper.rb
+++ app/helpers/i18n_helper.rb
@@ -1,4 +1,12 @@
 module I18nHelper
+	def i18n_common item
+		t("common.#{item}")
+	end
+
+	def i18n_skills type, item
+		t("skills.#{type}.#{item}")
+	end
+
 	def i18n_user_label item
 		t("user.label.#{item}")
 	end
